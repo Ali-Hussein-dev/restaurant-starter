da629aeeb1c8717a26e51983e3bca92bbd056b20
Ali <ali.hussein.pre@gmail.com>
2022-08-28 19:45:03 +0200

    feat: tweak layout and UI

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -7,12 +7,25 @@ const images = [
   { src: "/gallery/3.jpeg" },
   { src: "/gallery/4.jpeg" },
 ];
+/**
+ * with Tabs
+ * labels: eg. terrace, inside, outside
+ *
+ * without Tabs
+ */
 //======================================
 export const Gallery = () => {
   //======================================return
   return (
-    <section id="#gallery" className="w-full row-center element">
+    <section id="#gallery" className="w-full element">
+      <h2 className="h2">Our Premises</h2>
       <Carousel images={images} />
     </section>
   );
 };
+
+type TabsPropsT = {
+  labels: string[];
+  panels: React.ReactNode[];
+};
+const tabs = ({ labels, panels }: TabsPropsT) => {};
