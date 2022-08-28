ba20e6e33961f2e3f57333e1037b8c689e68c0ba
Ali <ali.hussein.pre@gmail.com>
2022-08-28 12:35:49 +0200

    refactor: add prop to image-bg

--- src/components/image-bg.tsx
+++ src/components/image-bg.tsx
@@ -3,17 +3,24 @@ import * as React from "react";
 //======================================
 export const ImageBg = ({
   src = "/falafel-fresh-salad.jpg",
+  brightness,
 }: {
   src?: string;
+  /**
+   * TW class eg: brightness-[0.3]
+   */
+  brightness?: string;
 }) => {
   //======================================return
   return (
-    <div className="absolute inset-0 grid w-full h-full">
+    <div className="absolute inset-0 w-full h-full">
       <picture>
         <img
           src={src}
           alt="background"
-          className="z-0 w-full h-full object-cover brightness-50"
+          className={`z-0 w-full h-full object-cover ${
+            brightness ? brightness : "brightness-50"
+          }`}
           loading="eager"
         />
       </picture>
