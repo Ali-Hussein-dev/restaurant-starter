eedf53170273788df710993b143be81db2025fb2
Ali <ali.hussein.pre@gmail.com>
2022-08-29 11:02:05 +0200

    chore: add next plugin

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const nextTranslate = require("next-translate");
+const withPlugins = require("next-compose-plugins");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPlugins([nextTranslate], nextConfig);
