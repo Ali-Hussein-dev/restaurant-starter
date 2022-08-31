d8602900351e73eb4496b357e59e7ec3f6ed1c41
Ali <ali.hussein.pre@gmail.com>
2022-08-31 17:49:49 +0200

    feat: name default theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" data-theme="black">
       <Head>
         <meta
           content="eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw"
