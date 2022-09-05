3b16ee66d066ae1a3ff11d62020aacd20daa3da8
Ali <ali.hussein.pre@gmail.com>
2022-09-05 23:31:32 +0200

    style: tweak UI

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,11 +8,14 @@ export const Form = () => {
   const { t } = useTranslation();
   //======================================return
   return (
-    <section id="#form" className="px-4 lg:px-1 py-28 element">
-      <div className="mx-auto border border-b-8 border-l-8 rounded-sm md:px-3 py-14 lg:w-4/6 bg-base-200">
+    <section
+      id="#form"
+      className="container px-4 mx-auto lg:px-1 py-28 element"
+    >
+      <div className="mx-auto border border-b-8 border-l-8 rounded-sm md:row-center md:px-3 py-14 lg:w-4/6 bg-base-200">
         <div className="max-w-4xl px-6 pt-4 col-center gap-y-10 md:flex-row md:justify-between md:gap-x-14 h-4/6">
-          <div className="p-16 bg-base-300/80 rounded-[25%_75%_75%_25%_/_80%_27%_73%_20%]">
-            <div className="relative aspect-square h-52">
+          <div className="p-12 md:p-16 bg-base-300/80 rounded-[25%_75%_75%_25%_/_80%_27%_73%_20%]">
+            <div className="relative h-40 aspect-square md:h-52">
               <Image
                 layout="fill"
                 src="/mail.svg"
