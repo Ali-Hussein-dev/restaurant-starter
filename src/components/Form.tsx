a8efd1277a0ec6f685917fbd71838908c848eca4
Ali <ali.hussein.pre@gmail.com>
2022-09-05 23:21:03 +0200

    style: tweak layout

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,8 +8,8 @@ export const Form = () => {
   const { t } = useTranslation();
   //======================================return
   return (
-    <section id="#form" className="px-1  py-28 element">
-      <div className="px-3 mx-auto border border-b-8 border-l-8 rounded-sm py-14 lg:w-4/6 bg-base-200">
+    <section id="#form" className="px-4 lg:px-1 py-28 element">
+      <div className="mx-auto border border-b-8 border-l-8 rounded-sm md:px-3 py-14 lg:w-4/6 bg-base-200">
         <div className="max-w-4xl px-6 pt-4 col-center gap-y-10 md:flex-row md:justify-between md:gap-x-14 h-4/6">
           <div className="p-16 bg-base-300/80 rounded-[25%_75%_75%_25%_/_80%_27%_73%_20%]">
             <div className="relative aspect-square h-52">
@@ -25,7 +25,7 @@ export const Form = () => {
           <div className="space-y-2">
             <div className="mb-4">
               <h3 className="mb-2 h3 text-primary">Get in Touch</h3>
-              <p className="w-11/12 mx-auto text-center md:w-10/12">
+              <p className="mx-auto text-center md:w-10/12">
                 Ut possimus qui ut temporibus culpa velit eveniet modi omnis est
                 adipisci expedita at atque vitae autem.
               </p>
