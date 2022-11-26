a25d488f4ca0a9abebe1460c690749cf595b89de
Ali <ali.hussein.pre@gmail.com>
2022-11-26 12:30:55 +0100

    fix: fix redirect not working

--- next.config.js
+++ next.config.js
@@ -2,21 +2,34 @@ const nextTranslate = require("next-translate");
 const withPlugins = require("next-compose-plugins");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = (phase, config) => ({
-  ...config,
+// const nextConfig = (phase, config) => ({
+//   ...config,
+//   reactStrictMode: true,
+//   i18n: { locales: ["en-US", "de"], defaultLocale: "en-US" },
+//   redirects() {
+//     return [
+//       {
+//         source: "/:id",
+//         destination: "https://restaurants-template.vercel.app/",
+//         permanent: true,
+//         basePath: false,
+//       },
+//     ];
+//   },
+// });
+
+// module.exports = withPlugins([nextTranslate], nextConfig);
+module.exports = {
   reactStrictMode: true,
   i18n: { locales: ["en-US", "de"], defaultLocale: "en-US" },
-
-  async redirects() {
+  redirects() {
     return [
       {
         source: "/:id",
-        destination: "https://restaurants-template.vercel.app/", // Matched parameters can be used in the destination
+        destination: "https://restaurants-template.vercel.app/",
         permanent: true,
         basePath: false,
       },
     ];
   },
-});
-
-module.exports = withPlugins([nextTranslate], nextConfig);
+};
